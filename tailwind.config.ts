591cc8c74d4409de0a3c4a784e737aeebe8167e7
joshbraham <josh27ofto@live.co.uk>
2023-12-11 21:20:06 +0000

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,33 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+
+// tablet: 768px = md
+// desktop: xl < 1440px < 2xl
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#d87d4a",
+        "primary-light": "#fbaf85",
+        "neutral-925": "#101010",
+        "neutral-150": "#f1f1f1",
+        // "neutral-50": '#fafafa', (native tailwind colour)
+      },
+      lineHeight: {
+        "11": "2.75rem",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "hero-mobile": "url('/img/home/mobile/image-hero.jpg')",
+        "hero-tablet": "url('/img/home/tablet/image-hero.jpg')",
+        "hero-desktop": "url('/img/home/desktop/image-hero.jpg')",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
+
